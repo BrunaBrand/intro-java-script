80dbf7fa0952c0dfa1e8f704b74e31ff666e7822
Bruna Brandão <bruna.costab@hotmail.com>
2022-06-28 19:11:02 -0300

    convertendo string em number

---
+++ script.js
@@ -0,0 +1,15 @@
+/* 
+
+Capturar 2 num
+e fazer as operações matematicas de soma,
+subtração, multiplicação, divisão e resto da divisão
+
+e para cada operação, mostrar um alerta com o resultado
+
+*/
+let firstNumber = prompt('Digite o primeiro número: ')
+let secondNumber = prompt('Digite o segundo número: ')
+
+//convetendo string em number
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
