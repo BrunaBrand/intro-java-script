d23408f797e59f9ddf96c0110c2ead8373bfda3f
Bruna Brandão <bruna.costab@hotmail.com>
2022-07-04 16:01:18 -0300

    item

--- script.js
+++ script.js
@@ -4,8 +4,9 @@
   Após capturar os 10 itens, imprima-os, separando por vírgula.
 */
 
-let itemName
 //variáveis criadas dentro do laço não são visíveis para os comandos que estão fora
 for (let item = 0; item < 10; item++) {
   itemName = prompt('Digite o item' + (item + 1))
 }
+
+;[]
