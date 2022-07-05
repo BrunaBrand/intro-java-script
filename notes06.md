a21d3450ad23a150bb748b3523f26b98f990439c
Bruna Brandão <bruna.costab@hotmail.com>
2022-07-05 17:40:58 -0300

    final do algoritmo

---
+++ notes06.md
@@ -0,0 +1,13 @@
+o que vamos ver na aula de hoje
+
+[] controlar o fluxo da aplicação
+[] estrutura de repetição
+-While
+
+[] Gerar número aleatório
+[] tipo de dado: NaN
+
+[] console
+
+- pacote com funcionalidades para o console devtools
+  math.round(arredonda numeros tanto para cima como para baixo) - Math.ceil("" para cima) - Math.floor("" para baixo)
---
+++ script.js
@@ -0,0 +1,31 @@
+/* 
+** Jogo da advinhaçao **
+
+Apresente a mensagem ao usuário:
+
+"Advinhe o número que estou pensando? Está entre 0 e 10"
+
+Crie uma lógica para gerar um número aleatório e verificar se o número digitado é o mesmo que o aleatório gerado pelo sistema.
+
+Enquanto o usuário não advinhar o número, mostrar a mensagem: "Erro, tente novamente!"
+
+Caso o usuário acerte o número, apresentar a mensagem: "Parabéns! Você advinhou o número em x tentativas"
+
+Substitua o "x" da mensagem, pelo número de tentativas"
+*/
+
+let result = prompt('Advinhe o número que estou pensando? Está entre 0 e 10')
+
+//Math conjuto de diversas funcionalidades matemáticas
+//ao colocar um ponto no final de cada declaração, ele abre um menu funcionalidades para trabalhar.
+//random é uma função
+const randomNumber = Math.round(Math.random() * 10)
+
+let xAttempts = 1
+
+while (Number(result) != randomNumber) {
+  result = prompt('Erro, tente novamente!')
+  xAttempts++
+}
+//interpolação de variável e string | template strings
+alert(`Parabéns! Você advinhou o número em ${xAttempts} tentativas`)
