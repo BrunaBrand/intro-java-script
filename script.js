72baad9a0f2fac6af0531b0f5f48804d87bcafb6
Bruna Brandão <bruna.costab@hotmail.com>
2022-07-04 21:02:12 -0300

    att

--- script.js
+++ script.js
@@ -5,8 +5,14 @@
 */
 
 //variáveis criadas dentro do laço não são visíveis para os comandos que estão fora
-for (let item = 0; item < 10; item++) {
-  itemName = prompt('Digite o item' + (item + 1))
-}
 
-;[]
+let itens = []
+//sempre que quiser acessar um array, comece pela posição 0, 1, 2 ......
+//arrays precisam começar da posição 0
+
+/*estrutura de repetição*/ /*variável de controle ja inicia com 0*/ /*enquanto*/ /*pegue o item e adc +1*/
+for (let item = 0; item < 10; item++) {
+  let itemName = prompt('Digite o item' + (item + 1))
+  itens[item] = itemName
+}
+alert(itens)
