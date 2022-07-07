9af26c283c9527fb5273e884593f2b6f5d975a5d
Bruna Brandão <bruna.costab@hotmail.com>
2022-07-07 18:36:17 -0300

    criações de função

--- 08/script.js
+++ 08/script.js
@@ -10,6 +10,18 @@
   Escreva uma lista contendo o nome dos pacientes
 */
 
+/* 
+  Dada uma lista de pacientes, descubra o IMC de cada paciente e imprima
+
+  "Paciente X possui o IMC de: Y"
+
+  Onde X é o nome do paciente e Y é o IMC desse paciente
+
+  Crie uma função para fazer o cálculo de IMC
+*/
+
+/* peso / (altura * altura) */
+
 //criando objetos no js, ele não pode ficar solto assim na pag, precisa de uma variável para pertencer
 //essa maneira de criar se chama de object literal, colocando na mão cada uma das propriedades do objeto smartp
 
@@ -33,13 +45,34 @@ const patients = [
     height: 180
   }
 ]
-//quando se coloca um ponto, ele abre várias opções de métodos e propriedades
-let patientsName = []
 
-//para 1 paciente de pacientes faça um bloco de ação
+//criação da função
+// function printPatientIMC(patients) {
+//   return `
+//   Paciente ${patients.name} possui o IMC de ${(
+//     patients.weight /
+//     (patients.height / 100) ** 2
+//   ).toFixed(2)}
+// `
+// }
 
-for (let patient of patients) {
-  patientsName.push(patient.name)
+//executando a função
+
+//outra maneira
+function IMC(weight, height) {
+  return (weight / (height / 100) ** 2).toFixed(2)
 }
 
-alert(patientsName)
+function printPatientIMC(patients) {
+  return `
+  Paciente ${patients.name} possui o IMC de ${IMC(
+    patients.weight,
+    patients.height
+  )}
+    `
+}
+//o for é usado nas duas maneiras para continuar executando o programa.
+for (let patient of patients) {
+  let IMCmessage = printPatientIMC(patient)
+  alert(IMCmessage)
+}
