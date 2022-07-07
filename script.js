de8e99c2be14526afa83d78332476d3a750d3f97
Bruna Brandão <bruna.costab@hotmail.com>
2022-07-07 16:59:40 -0300

    primeira declaração do objeto[C

---
+++ script.js
@@ -0,0 +1,35 @@
+/* 
+  Crie uma lista de pacientes
+
+  Cada paciente dentro da lista, deverá conter
+    nome
+    idade
+    peso
+    altura
+
+  Escreva uma lista contendo o nome dos pacientes
+*/
+
+//criando objetos no js, ele não pode ficar solto assim na pag, precisa de uma variável para pertencer
+//essa maneira de criar se chama de object literal, colocando na mão cada uma das propriedades do objeto smartp
+
+const pacients = [
+  {
+    name: 'Luiz',
+    age: 20,
+    weight: 100,
+    height: 190
+  },
+  {
+    name: 'Alexandra',
+    age: 27,
+    weight: 70,
+    height: 170
+  },
+  {
+    name: 'Carlos',
+    age: 42,
+    weight: 90,
+    height: 180
+  }
+]
